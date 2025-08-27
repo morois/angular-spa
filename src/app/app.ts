2a9bee190e57099bdcb78a6c8d6ccf66fc828ccb
Andrew Morois <ehot.tohet@gmail.com>
2025-08-27 15:21:43 +0300

    Initial Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCard } from './common-ui/profile-card/profile-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCard],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('tik-talk');
